867f2206a8da63268ea59860ad3825c05650d141
Yanet Roman <yanet_0688@yahoo.com>
2023-03-31 19:29:52 +0000

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    #write a ton of code
+    #render or redirect
+    #redirect_to("http://www.wikipedia.org")
+    render({:template => "game_templates/user_rock.html.erb"})
+  end
 end
