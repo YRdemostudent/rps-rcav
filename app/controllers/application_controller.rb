b143098f8b00e7e7f343c3d63fbb828441b43e85
Yanet Roman <yanet_0688@yahoo.com>
2023-04-04 05:39:25 +0000

    added links to RPS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  layout("wrapper.html.erb")
+  layout(false)
 
   # Add your actions below this line
   # ================================
 
   def homepage
-    render({ :template => "game_templates/rules.html.erb" })
+    
+    render({ :template => "game_templates/rules.html.erb", :layout => "wrapper.html.erb" })
   end
 
   def play_rock
@@ -21,34 +22,34 @@ class ApplicationController < ActionController::Base
     elsif @comp_move == "scissors"
       @outcome = "won"
     end
-    render({ :template => "game_templates/user_rock.html.erb" })
+    render({ :template => "game_templates/user_rock.html.erb", :layout => "wrapper.html.erb"})
   end
 
   def play_paper
     @comp_move = ["rock", "paper", "scissors"].sample
 
     if @comp_move == "rock"
-      @outcome = "tied"
-    elsif @comp_move == "paper"
-      @outcome = "lost"
-    elsif @comp_move == "scissors"
       @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
     end
 
-    render({ :template => "game_templates/user_paper.html.erb" })
+    render({ :template => "game_templates/user_paper.html.erb", :layout => "wrapper.html.erb" })
   end
 
   def play_scissors
     @comp_move = ["rock", "paper", "scissors"].sample
 
     if @comp_move == "rock"
-      @outcome = "won"
-    elsif @comp_move == "paper"
-      @outcome = "tied"
-    elsif @comp_move == "scissors"
       @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
     end
 
-    render({ :template => "game_templates/user_scissors.html.erb" })
+    render({ :template => "game_templates/user_scissors.html.erb", :layout => "wrapper.html.erb" })
   end
 end
