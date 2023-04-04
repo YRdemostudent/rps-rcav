be06bd43bd17f503f21faa020cc044955f7edb74
Yanet Roman <yanet_0688@yahoo.com>
2023-04-04 04:41:28 +0000

    scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,54 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
-  
+
   def homepage
-    render({:template => "game_templates/rules.html.erb"})
-  end 
+    render({ :template => "game_templates/rules.html.erb" })
+  end
 
   def play_rock
     #write a ton of code
     #render or redirect
     #redirect_to("http://www.wikipedia.org")
-    render({:template => "game_templates/user_rock.html.erb"})
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/user_rock.html.erb" })
   end
 
   def play_paper
     @comp_move = ["rock", "paper", "scissors"].sample
-    
-if @comp_move == "rock"
-  @outcome = "tied"
-elsif @comp_move == "paper"
-  @outcome = "lost"
-elsif @comp_move == "scissors"
-  @outcome = "won"
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end
 end
-
-  render({:template => "game_templates/user_paper.html.erb"})
-  end 
-
-  def play_scissors 
-
-    render({:template => "game_templates/user_scissors.html.erb"})
-  end 
-end 
